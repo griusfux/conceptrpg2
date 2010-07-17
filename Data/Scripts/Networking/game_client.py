82db8cd67e6210d083df590996bbdef2700b5fbf
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2010-07-17 07:01:37 +0000

    Forgot a couple of files...

---
+++ Data/Scripts/Networking/game_client.py
@@ -0,0 +1,67 @@
+# $Id: $
+
+# Description: The game client
+# Contributers: Mitchell Stokes
+
+from Scripts.Networking import NET_ENCODING
+
+import socket
+import time
+
+# The buffer size to use
+BUFFER = 4096
+TIMEOUT = 20
+
+class GameClient:
+	"""The client for game networking"""
+	
+	def __init__(self, id, addr):
+		self.id = id
+		self.addr = addr
+		
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.socket.setblocking(0)
+		
+		self.connected = False
+		self.server_addr = None
+		
+		self.last_update = time.time()
+		self.socket.sendto(bytes(id, NET_ENCODING), addr)
+		
+	def run(self):
+		"""Try to get data from the server"""
+		
+		val = None
+		
+		try:
+			data, addr = self.socket.recvfrom(BUFFER)
+			
+			if not self.server_addr:
+				print("Server found")
+				print(data, addr)
+				self.last_update = time.time()
+				self.server_addr = addr
+				self.connected = True
+			elif self.server_addr == addr:
+				self.last_update = time.time()
+				data = str(data, NET_ENCODING).split()
+				val = (data[0], data[1:])
+
+		except socket.error:
+			pass
+							
+		if time.time() - self.last_update > TIMEOUT:
+			print("Connection to the server timed out")
+			self.server_addr = "0.0.0.0"
+			self.connected = False
+			
+		return val
+		
+	def send(self, msg):
+		"""Send a message and user name to the server"""
+		
+		if not self.connected:
+			return
+		
+		self.socket.sendto(bytes(self.id+" "+msg, NET_ENCODING), self.server_addr)
+				
---
+++ Data/Scripts/Networking/game_server.py
@@ -0,0 +1,114 @@
+# $Id: $
+
+# Description: The game server
+# Contributers: Mitchell Stokes
+
+# from socketserver import UDPServer, BaseRequestHandler
+
+from Scripts.Networking import NET_ENCODING
+from Scripts.gamestates import *
+
+import socket
+import select
+import time
+
+BUFFER = 4096
+
+# class GameRequestHandler(BaseRequestHandler):
+	# """Handles incoming requests"""
+	
+class ClientHandle():
+	"""Class for handling client requests"""
+	
+	def __init__(self, server, client_addr):
+		self.server = server
+		self.last_update = time.time()
+		self.addr = client_addr
+		
+	def handle_request(self, data, client_addr):
+		self.last_update = time.time()
+		self.addr = client_addr
+		
+		if data:
+			# Clean up the data a little
+			self.id = data.split()[0]
+			self.data = "".join([i + " " for i in data.split()[1:]])
+		
+			print("Message %s from %s" % (data, client_addr))
+			self.server.main['state'].run(self, self.server.main)
+
+class GameServer():
+	"""The game server"""
+	
+	def __init__(self, port, timeout):
+		#UDPServer.__init__(self, ('', port), GameRequestHandler)
+				
+		# The "main" dict for storing globals
+		self.main = {}
+		
+		# Client info
+		self.main['clients'] = {}
+
+		# Set the default state
+		self.main['state'] = DefaultState(self.main, True)
+		
+		# Create the socket
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.socket.bind(('', port))
+		
+		# How long we wait on players
+		self.timeout = timeout
+		
+		print("Server ready")
+		
+		# Now run the server
+		while True:
+			r, w, e = select.select([self.socket], [], [], 0.01)
+			if r:
+				try:
+					data, client_addr = self.socket.recvfrom(BUFFER)
+					data = str(data, NET_ENCODING)
+				except socket.error as e:
+					print(e)
+					data = ""
+				
+				if data:
+					client_id = data.split()[0]
+					
+					print(client_addr)
+					if client_id not in self.main['clients']:
+						self.register_client(client_id, client_addr)
+					elif self.main['clients'][client_id].addr != client_addr:
+						# Make the client_id unique
+						while client_id in self.main['clients']:
+							client_id = client_id + "_"
+							
+						self.register_client(self, client_id, client_addr)
+						
+					self.main['clients'][client_id].handle_request(data, client_addr)
+				
+			# Check the status of the clients
+			for cid in [i for i in self.main['clients'].keys()]:
+				if time.time() - self.main['clients'][cid].last_update > self.timeout:
+					self.broadcast(cid + " to")
+					self.drop_client(cid, "Timed Out")
+					
+	def register_client(self, client_id, client_addr):
+		"""Registers a client"""
+		
+		print(client_id, "Registered")
+		self.main['clients'][client_id] = ClientHandle(self, client_addr)
+					
+	def drop_client(self, client_id, reason):
+		"""Drop a client"""
+		
+		print(client_id, reason)
+		del self.main['clients'][client_id]
+		
+	def broadcast(self, data):
+		"""Broadcast a message to all of the clients"""
+		
+		print("BROADCAST:", data)
+		
+		for cid, client in self.main['clients'].items():
+			self.socket.sendto(bytes(data, NET_ENCODING), client.addr)
