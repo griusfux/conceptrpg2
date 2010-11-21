bc40f9a17852189cb8d56f8faf7767cf25dba263
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2010-11-21 23:12:43 +0000

    ==Items== *Added images to the item packages

--- Data/Scripts/packages.py
+++ Data/Scripts/packages.py
@@ -366,6 +366,7 @@ class Item(Package):
 	_schema = 'Schemas/itemfile.json'
 	_new = 'Schemas/itemfile_new.json'
 	_dir = 'Items/Others'
+	_img = 'Items/Others/item.png'
 	
 class Weapon(Item):
 	"""Weapon Package"""
@@ -375,6 +376,7 @@ class Weapon(Item):
 	_parent_schema = Item._schema
 	_schema = 'Schemas/weaponfile.json'
 	_dir = 'Items/Weapons'
+	_img = 'Items/Weapons/item.png'
 	
 	def __init__(self, package_name):
 		Item.__init__(self, package_name)
@@ -405,6 +407,7 @@ class Armor(Item):
 	_parent_schema = Item._schema
 	_schema = 'Schemas/armorfile.json'
 	_dir = 'Items/Armors'
+	_img = 'Items/Armors/item.png'
 	
 class ActionSet(Package):
 	"""Action Set Package"""
