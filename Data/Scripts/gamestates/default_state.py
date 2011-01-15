f19d19852cc6af2dd1d221a25668528d24103381
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-01-15 05:47:28 +0000

    Stop sending position updates to the server for now as these seems to cause movement issues.

--- Data/Scripts/gamestates/default_state.py
+++ Data/Scripts/gamestates/default_state.py
@@ -162,7 +162,7 @@ class DefaultState(BaseState, BaseController):
 		# Normalize the vector to the character's speed
 		if movement != [0.0, 0.0, 0.0]:
 			movement = [float(i) for i in (Vector(movement).normalize()*speed)]
-			self.server.invoke("position", id, *main['player'].object.position)
+			# self.server.invoke("position", id, *main['player'].object.position)
 		
 		# Otherwise, idle
 		else:
