5fd80be3f025095279b1d77cfdd0e7ebde276010
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-05-28 23:26:45 +0000

    Updating pyuic script to run Python32 instead of Python31

--- Data/EditorFiles/Editors/ui/pyuic.py
+++ Data/EditorFiles/Editors/ui/pyuic.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
 	file = sys.argv[1].split('\\')[-1]
 
-	subprocess.call(r"python C:\Python31\Lib\site-packages\PyQt4\uic\pyuic.py -o "\
+	subprocess.call(r"python C:\Python32\Lib\site-packages\PyQt4\uic\pyuic.py -o "\
 					+ file[:-3]+"_ui.py " +file)
