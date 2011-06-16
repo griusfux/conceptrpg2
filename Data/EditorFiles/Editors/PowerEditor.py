98b1265ebc35d89736798bdda7e41e7419127ed8
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-06-16 05:21:40 +0000

    Updating the power editor and power schema.

--- Data/EditorFiles/Editors/PowerEditor.py
+++ Data/EditorFiles/Editors/PowerEditor.py
@@ -15,17 +15,36 @@ class PowerEditor(QFrame):
 		
 		# Load up the schema file so we can get acceptable subtypes
 		with open(data._schema) as f:
-			range_type = eval(json.loads(f.read())['range_type'])
+			schema = json.loads(f.read())
+			element = eval(schema['element'])
+			delivery = eval(schema['delivery'])
+			effect_shape = eval(schema['effect_shape'])
+			
 		
-		self.ui.range_type.addItems([i for i in range_type])
+		self.ui.element.addItems([i.title() for i in element])
+		self.ui.delivery.addItems([i.title() for i in delivery])
+		self.ui.effect_shape.addItems([i.title() for i in effect_shape])
 		
 		# Set the input fields
 		self.ui.name.setText(data.name)
-		self.ui.range_type.setCurrentIndex(self.ui.range_type.findText(data.range_type))
-		self.ui.range_size.setValue(data.range_size)
+		self.ui.element.setCurrentIndex(self.ui.element.findText(data.element.title()))
+		self.ui.delivery.setCurrentIndex(self.ui.delivery.findText(data.delivery.title()))
+		self.ui.effect_shape.setCurrentIndex(self.ui.effect_shape.findText(data.effect_shape.title()))
+		self.ui.shape_modifier.setValue(data.shape_modifier)
+		self.ui.distance.setValue(data.distance)
+		self.ui.tier.setValue(data.tier)
+		self.ui.cool_down.setValue(data.cool_down)
+		self.ui.description.setPlainText(data.description)
+		
 		
 	def save(self):
 		self.data.name = self.ui.name.text()
-		self.data.range_type = self.ui.range_type.itemText(self.ui.range_type.currentIndex())
-		self.data.range_size = self.ui.range_size.value()
+		self.data.element = self.ui.element.itemText(self.ui.element.currentIndex()).upper()
+		self.data.delivery = self.ui.delivery.itemText(self.ui.delivery.currentIndex()).upper()
+		self.data.effect_shape = self.ui.effect_shape.itemText(self.ui.effect_shape.currentIndex()).upper()
+		self.data.shape_modifier = self.ui.shape_modifier.value()
+		self.data.distance = self.ui.distance.value()
+		self.data.tier = self.ui.tier.value()
+		self.data.cool_down = self.ui.cool_down.value()
+		self.data.description = self.ui.description.toPlainText()		
 		
