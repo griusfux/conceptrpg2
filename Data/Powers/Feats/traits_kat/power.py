7971ecccb5133864b2aae521061087fd6c5e9079
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-06-19 04:32:52 +0000

    Fixing some level up bugs and giving the editor a new icon.

--- Data/Powers/Feats/traits_kat/power.py
+++ Data/Powers/Feats/traits_kat/power.py
@@ -3,12 +3,8 @@ def power(self, controller, user):
 		
 def push(self, controller, user):
 
-	user.unspent_levels[0].ability_points += 4
-	user.unspent_levels[0].feats += 1
-	user.unspent_levels[0].at_will_count += 1
-	
-	for stat in ('fortitude', 'reflex', 'will'):
-		controller.modify_stat(user, stat, 1)
+	user.unspent_levels[0].ap += 1
+	user.unspent_levels[0].pp += 2
 	
 def pop(self, controller, user):
 	print("Kat racial traits unloaded?! >:(")
--- Data/Scripts/levels.py
+++ Data/Scripts/levels.py
@@ -5,13 +5,13 @@ class UnspentLevel:
 	
 		self.ap = 1
 		self.pp = 1
-		sefl.dp = 0
+		self.dp = 0
 		self.apply_affinities = False
 		
 		if level == 1:
 			self.ap = 0
 			self.pp = 3
-			sefl.apply_affiniies = True
+			self.apply_affiniies = True
 		elif level % 5 == 0:
 			self.apply_affinities = True
 			self.dp = 1
