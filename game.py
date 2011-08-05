9ce481dc60891d5570be5fb8f47e275d1b067f34
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-08-05 02:48:01 +0000

    Removing some __pycache__ folders from the win64 release libs.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call("release/win64/blenderplayer.exe -w 1280 800 -m 4 -g show_framerate = 0 -g show_profile = 0 Data/data.blend",
+subprocess.call("release/win64/blenderplayer.exe -w 1280 800 -m 4 -g show_framerate = 1 -g show_profile = 1 Data/data.blend",
 				creationflags=subprocess.CREATE_NEW_CONSOLE)
