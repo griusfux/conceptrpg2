e847dfd11a563635875d5993c126e326a41eaa34
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-08-11 23:41:27 +0000

    Updating Attack/power.py to a change in the controller.

--- Data/Powers/Powers/Attack/power.py
+++ Data/Powers/Powers/Attack/power.py
@@ -1,6 +1,6 @@
 def power(self, controller, user):
 
-	controller.animate_attack(user, "1h Swing")
+	controller.animate_weapon(user, "1h Swing")
 
 	for target in user.targets:
 		controller.modify_health(target, -10)
