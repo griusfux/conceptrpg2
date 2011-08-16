7a3df8e4e37bcd7b66c19170712ca615996e22a3
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-08-16 00:32:11 +0000

    Adding the SFX and functionality for the Back Stab power.

---
+++ Data/Powers/Powers/Back Stab/power.py
@@ -0,0 +1,27 @@
+import Scripts.effects as Effect
+import Scripts.packages as Package
+
+def power(self, controller, user):
+	targets = controller.get_targets(self, user)
+	if targets == []:
+		return
+	target = targets[0]
+	
+	pos = user.object.position
+	effect = Effect.StaticEffect("small_smoke", pos, 50)
+	controller.add_effect(effect)
+	
+	new_pos = target.object.position
+	new_pos = new_pos - target.object.forward_vector
+	new_pos *= user.weapon.range
+	controller.reposition(user, new_pos)
+	rotation = user.object.forward_vector.rotation_difference(target.object.forward_vector)
+	rotation = rotation.to_euler()
+	controller.move(user, angular=rotation)
+	
+	
+	pos = user.object.position
+	effect = Effect.StaticEffect("small_smoke", pos, 50)
+	controller.add_effect(effect)
+	
+	controller.attack(self, user, multiplier=2)
