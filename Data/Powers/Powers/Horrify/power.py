1a597f91e8f122d1b0f34703a6f627accf4e218b
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-08-17 04:13:57 +0000

    Adding the SFX and functionality for the Horrify power. (The statuses used by the Horrify power is not yet functional)

---
+++ Data/Powers/Powers/Horrify/power.py
@@ -0,0 +1,21 @@
+import Scripts.effects as Effect
+
+def power(self, controller, user):
+	targets = controller.get_targets(self, user)
+	if targets == []:
+		return
+	target = targets[0]
+	
+	controller.animate_spell(user, "cast")
+
+	for target in targets:
+		amount = -.1 * self.tier
+		controller.add_status(target, "Reflex", amount, 2)
+		controller.add_status(target, "Arcane Defense", amount, 2)
+		
+		pos = target.object.position
+		effect = Effect.StaticEffect("small_smoke", pos, 50)
+		controller.add_effect(effect)
+		pos = target.object.position
+		effect = Effect.StaticEffect("skull_smoke", pos, 120)
+		controller.add_effect(effect)
