bc14e51f00c1b990b6ed28d802612980de2db543
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-08-21 19:56:56 +0000

    Adding the following to the MonsterEditor:   * Image   * Root Object   * Action Set

--- Data/EditorFiles/Editors/MonsterEditor.py
+++ Data/EditorFiles/Editors/MonsterEditor.py
@@ -1,14 +1,36 @@
 from PyQt4.QtGui import *
 from .BaseEditor import BaseEditor
 from .ui.MonsterEditor_ui import Ui_MonsterEditor
+from ..common import get_blender_objects
+
+from Scripts.packages import ActionSet
 
 class MonsterEditor(BaseEditor):
 	def __init__(self, parent, data):
 		BaseEditor.__init__(self, parent, data, Ui_MonsterEditor)
 		ui = self.ui
+				
+		# Fill combo boxes
+		ui.root_object.addItems(get_blender_objects(data))
+		ui.action_set.addItems([i.name for i in ActionSet.get_package_list()])
+		
+		# Setup the image
+		image = QPixmap(data.open_image())
+		data.close_image()
+		ui.monster_image.setPixmap(image)
 		
 		# Setup the values
 		ui.name.setText(data.name)
+		idx = ui.root_object.findText(data.root_object)
+		if idx < 0:
+			ui.root_object.setEditText(data.root_object)
+		else:
+			ui.root_object.setCurrentIndex(ui.root_object.findText(data.root_object))
+		ui.action_set.setCurrentIndex(ui.action_set.findText(data.action_set))
 		
 	def save(self):
 		self.data.name = self.ui.name.text()
+				
+		data.name = ui.name.text()
+		data.root_object = ui.root_object.currentText()
+		data.action_set = ui.action_set.currentText()
--- Data/EditorFiles/Editors/ui/MonsterEditor_ui.py
+++ Data/EditorFiles/Editors/ui/MonsterEditor_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MonsterEditor.ui'
 #
-# Created: Sun Aug 21 00:56:40 2011
+# Created: Sun Aug 21 12:56:25 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -18,8 +18,8 @@ class Ui_MonsterEditor(object):
     def setupUi(self, MonsterEditor):
         MonsterEditor.setObjectName(_fromUtf8("MonsterEditor"))
         MonsterEditor.resize(400, 300)
-        self.verticalLayout = QtGui.QVBoxLayout(MonsterEditor)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.formLayout = QtGui.QFormLayout(MonsterEditor)
+        self.formLayout.setObjectName(_fromUtf8("formLayout"))
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.label = QtGui.QLabel(MonsterEditor)
@@ -28,9 +28,31 @@ class Ui_MonsterEditor(object):
         self.name = QtGui.QLineEdit(MonsterEditor)
         self.name.setObjectName(_fromUtf8("name"))
         self.gridLayout.addWidget(self.name, 0, 1, 1, 1)
-        self.verticalLayout.addLayout(self.gridLayout)
         spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem)
+        self.gridLayout.addItem(spacerItem, 3, 0, 1, 1)
+        self.label_3 = QtGui.QLabel(MonsterEditor)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+        self.root_object = QtGui.QComboBox(MonsterEditor)
+        self.root_object.setEditable(True)
+        self.root_object.setObjectName(_fromUtf8("root_object"))
+        self.gridLayout.addWidget(self.root_object, 1, 1, 1, 1)
+        self.action_set = QtGui.QComboBox(MonsterEditor)
+        self.action_set.setObjectName(_fromUtf8("action_set"))
+        self.gridLayout.addWidget(self.action_set, 2, 1, 1, 1)
+        self.label_4 = QtGui.QLabel(MonsterEditor)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.gridLayout.addWidget(self.label_4, 2, 0, 1, 1)
+        self.formLayout.setLayout(1, QtGui.QFormLayout.FieldRole, self.gridLayout)
+        self.monster_image = QtGui.QLabel(MonsterEditor)
+        self.monster_image.setMinimumSize(QtCore.QSize(150, 150))
+        self.monster_image.setMaximumSize(QtCore.QSize(150, 150))
+        self.monster_image.setFrameShape(QtGui.QFrame.Box)
+        self.monster_image.setLineWidth(2)
+        self.monster_image.setText(_fromUtf8(""))
+        self.monster_image.setScaledContents(True)
+        self.monster_image.setObjectName(_fromUtf8("monster_image"))
+        self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.monster_image)
 
         self.retranslateUi(MonsterEditor)
         QtCore.QObject.connect(self.name, QtCore.SIGNAL(_fromUtf8("textEdited(QString)")), MonsterEditor.modified)
@@ -39,4 +61,6 @@ class Ui_MonsterEditor(object):
     def retranslateUi(self, MonsterEditor):
         MonsterEditor.setWindowTitle(QtGui.QApplication.translate("MonsterEditor", "MonsterEditor", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MonsterEditor", "Name: ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("MonsterEditor", "Root Object: ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("MonsterEditor", "Action Set: ", None, QtGui.QApplication.UnicodeUTF8))
 
