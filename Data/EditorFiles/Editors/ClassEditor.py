da246c646c1099aeea0bfb73cd21d2c550377b80
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-09-28 03:19:54 +0000

    Adding starting_armor and starting_weapon to class files. The ClassEditor has also been updated and was used to set the starting_armor for the classes: Hunter = Leather, Mage = Robes, Warrior = Plate Mail.

--- Data/EditorFiles/Editors/ClassEditor.py
+++ Data/EditorFiles/Editors/ClassEditor.py
@@ -2,13 +2,27 @@ from PyQt4.QtGui import *
 from .BaseEditor import BaseEditor
 from .ui.ClassEditor_ui import Ui_ClassEditor
 
+from Scripts.packages import Armor, Weapon
+
 class ClassEditor(BaseEditor):
 	def __init__(self, parent, data):
 		BaseEditor.__init__(self, parent, data, Ui_ClassEditor)
 		ui = self.ui
 		
+		# Fill combo boxes
+		ui.starting_armor.addItems([i.name for i in Armor.get_package_list()])
+		ui.starting_weapon.addItems([i.name for i in Weapon.get_package_list()])
+		
 		# Set the text fields
 		ui.name.setText(data.name)
+		ui.starting_armor.setCurrentIndex(ui.starting_armor.findText(data.starting_armor))
+		ui.starting_weapon.setCurrentIndex(ui.starting_weapon.findText(data.starting_weapon))
 		
 	def save(self):
-		self.data.name = self.ui.name.text()
+		data = self.data
+		ui = self.ui
+
+		data.name = ui.name.text()
+		data.starting_armor = ui.starting_armor.currentText()
+		data.starting_weapon = ui.starting_weapon.currentText()
+		
--- Data/EditorFiles/Editors/ui/ClassEditor_ui.py
+++ Data/EditorFiles/Editors/ui/ClassEditor_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ClassEditor.ui'
 #
-# Created: Sun Aug 21 00:52:04 2011
+# Created: Tue Sep 27 20:18:27 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -28,15 +28,31 @@ class Ui_ClassEditor(object):
         self.name = QtGui.QLineEdit(ClassEditor)
         self.name.setObjectName(_fromUtf8("name"))
         self.gridLayout.addWidget(self.name, 0, 1, 1, 1)
+        self.label_2 = QtGui.QLabel(ClassEditor)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.starting_armor = QtGui.QComboBox(ClassEditor)
+        self.starting_armor.setObjectName(_fromUtf8("starting_armor"))
+        self.gridLayout.addWidget(self.starting_armor, 1, 1, 1, 1)
+        self.label_3 = QtGui.QLabel(ClassEditor)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.starting_weapon = QtGui.QComboBox(ClassEditor)
+        self.starting_weapon.setObjectName(_fromUtf8("starting_weapon"))
+        self.gridLayout.addWidget(self.starting_weapon, 2, 1, 1, 1)
         self.verticalLayout.addLayout(self.gridLayout)
         spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
 
         self.retranslateUi(ClassEditor)
         QtCore.QObject.connect(self.name, QtCore.SIGNAL(_fromUtf8("textEdited(QString)")), ClassEditor.modified)
+        QtCore.QObject.connect(self.starting_armor, QtCore.SIGNAL(_fromUtf8("currentIndexChanged(QString)")), ClassEditor.modified)
+        QtCore.QObject.connect(self.starting_weapon, QtCore.SIGNAL(_fromUtf8("currentIndexChanged(QString)")), ClassEditor.modified)
         QtCore.QMetaObject.connectSlotsByName(ClassEditor)
 
     def retranslateUi(self, ClassEditor):
         ClassEditor.setWindowTitle(QtGui.QApplication.translate("ClassEditor", "ClassEditor", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("ClassEditor", "Name:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("ClassEditor", "Starting Armor: ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("ClassEditor", "Starting Weapon: ", None, QtGui.QApplication.UnicodeUTF8))
 
