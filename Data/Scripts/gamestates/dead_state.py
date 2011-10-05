357d78b6fcaf45553a8b2ff7388b009481e7bf41
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-10-05 03:06:05 +0000

    Forgot to add the actual dead_state.py.

---
+++ Data/Scripts/gamestates/dead_state.py
@@ -0,0 +1,55 @@
+from .combat_state import CombatState
+
+class DeadState(CombatState):
+	"""Dead gamestate"""
+
+	client_functions = CombatState.client_functions.copy()
+	server_functions = CombatState.server_functions.copy()
+	
+	##########
+	# Client
+	##########
+	
+	def client_init(self, main):
+		"""Initialize the client state"""
+		
+		main['ui_system'].load_layout("dead")
+		
+		self.play_animation(main['player'], "Die")
+		
+	def client_run(self, main):
+		"""Client-side run method"""
+		
+		player = main['player']
+		inputs = main['input_system'].run()
+		
+		if inputs:
+			if ("UsePower", "INPUT_CLICK") in inputs or \
+				("Action", "INPUT_CLICK") in inputs:
+				player.position = main['dgen'].start_position
+				self.server.invoke("position", player.id, *player.position)
+				return ("Default", "SWITCH")
+			
+	def client_cleanup(self, main):
+		"""Cleanup the client state"""
+		pass
+			
+	##########
+	# Server
+	##########
+		
+	def server_init(self, main):
+		"""Initialize the server state"""
+		pass
+		
+	def server_run(self, main, client):
+		"""Server-side run method"""
+		pass
+			
+	def server_cleanup(self, main):
+		"""Cleanup the server state"""
+		pass
+
+	##########
+	# Other
+	##########
