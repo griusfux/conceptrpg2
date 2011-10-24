c08a7818c15ca6a096a3cbb2d4ee16ba0b6d4b78
Moguri <Moguri@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-10-24 20:47:58 +0000

    List all powers in the Power screen. This means that attack can now be swapped out.

--- Data/Scripts/ui/layouts.py
+++ Data/Scripts/ui/layouts.py
@@ -456,8 +456,7 @@ class PowersLayout(Layout):
 		if self.element != self.ele_bar.element:
 			self.element = self.ele_bar.element
 			self.powers = [power for power in Power.get_package_list()
-							if power.element == self.element.upper()
-							and	power.tier == 1]
+							if power.element == self.element.upper()]
 			self.lbox.items = self.powers
 			self.lbox.active = None
 				
