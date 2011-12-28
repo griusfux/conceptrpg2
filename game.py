f3774b6064be9f67d505aefd55b81651df88c174
Kupoman <Kupoman@f65051a6-2924-51df-83f0-b0952c0bc907>
2011-12-28 03:46:02 +0000

    Reverting game.py from an accidental commit.

--- game.py
+++ game.py
@@ -4,7 +4,6 @@ import configparser
 from collections import OrderedDict
 
 CONFIG_NAME = "config.ini"
-#CONFIG_NAME = "record_config.ini"
 PLATFORMS = ('win64')
 
 def main():
@@ -36,9 +35,7 @@ def main():
 		print("%s is not a supported platform at this time" % platform)
 		return
 	
-#	args.append("release/win64/blenderplayer.exe")
-#	args.append("D:/blender-dev/install/trunk-2.5/blenderplayer.exe")
-	args.append("D:/blender-dev/trunk/projectfiles/bin/RelWithDebInfo/blenderplayer.exe")
+	args.append("release/%s/blenderplayer.exe" % platform)
 	args.append('-f' if config.getboolean('window', 'fullscreen') else '-w')
 	args.append(config['window']['x_resolution'])
 	args.append(config['window']['y_resolution'])
@@ -51,8 +48,6 @@ def main():
 		
 	args.append("Data/data.blend")
 		
-#	with open("log1.txt", "w") as f:
-#		subprocess.call(" ".join(args), creationflags=subprocess.CREATE_NEW_CONSOLE, stdout=f, stderr=f)
 	subprocess.call(" ".join(args), creationflags=subprocess.CREATE_NEW_CONSOLE)
-
+	
 main()
